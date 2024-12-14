a427a35acc4f2720fe44fc7caefe76e1ee1b097e
pyr33x <pyrexrj@gmail.com>
2024-12-15 01:38:15 +0330

    🚀

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Todo
 
-A simple todo app with authentication, routing, and data fetching.
+A simple todo app with authentication and database.
 
-## Features
-
-- Authentication with GitHub
-- TypeScript
-- Tailwind CSS
-- Drizzle
-- Next.js
-- Shadcn
-
-## Getting Started
-
-Install the dependencies:
+## Local Development
 
 ```bash
 bun install
 ```
 
-First, run the development server:
+rename `.env.example` to `.env` and fill in the values.
 
 ```bash
 bun run dev
 ```
+
+## Stack
+
+- [Next.js](https://nextjs.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Vercel](https://vercel.com/)
+- [Drizzle](https://www.drizzle.team/)
+- [Neon](https://neon.tech/)
+- [Authjs](https://authjs.dev/)
+- [Shadcn](https://ui.shadcn.com)
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,44 +1,9 @@
-import { GithubIcon } from "~/components/icons";
-import { LayoutDashboard } from "lucide-react";
-import { Button } from "~/components/ui";
-import { auth } from "~/server/auth";
-import Link from "next/link";
+import { Hero } from "~/app/(home)/_components/hero";
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
   return (
     <section className="max-w-3xl px-4 lg:px-2 text-center">
-      <h1 className="text-2xl md:text-4xl font-bold text-foreground text-center">
-        Todo
-      </h1>
-      <p className="text-xl md:text-2xl text-balance max-w-lg mt-1 font-medium text-muted-foreground text-center">
-        A simple todo app with authentication and database.
-      </p>
-      <div className="mt-6 flex flex-row flex-wrap items-center justify-center gap-2">
-        <a
-          href="https://github.com/pyr33x/todo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button>
-            <GithubIcon className="mr-2 h-4 w-4" />
-            Star on Github
-          </Button>
-        </a>
-        {session ? (
-          <Link href="/dashboard">
-            <Button variant="secondary">
-              Dashboard
-              <LayoutDashboard className="ml-2 h-4 w-4" />
-            </Button>
-          </Link>
-        ) : (
-          <Button variant="secondary" disabled>
-            Dashboard
-            <LayoutDashboard className="ml-2 h-4 w-4" />
-          </Button>
-        )}
-      </div>
+      <Hero />
     </section>
   );
 }
