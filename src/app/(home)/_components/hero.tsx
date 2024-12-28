c9ab528447cb37f9b0f05a56b2021f459047e768
pyr33x <pyrexrj@gmail.com>
2024-12-28 21:00:35 +0330

    fix: change icon

--- src/app/(home)/_components/hero.tsx
+++ src/app/(home)/_components/hero.tsx
@@ -1,5 +1,5 @@
+import { LayoutDashboardIcon, StarIcon } from "lucide-react";
 import { GithubIcon } from "~/components/icons";
-import { LayoutDashboard } from "lucide-react";
 import { auth, signIn } from "~/server/auth";
 import { Button } from "~/components/ui";
 import Link from "next/link";
@@ -27,7 +27,7 @@ export async function Hero() {
           rel="noopener noreferrer"
         >
           <Button>
-            <GithubIcon className="mr-1 h-4 w-4" />
+            <StarIcon className="h-4 w-4" />
             Star on Github
           </Button>
         </a>
@@ -35,13 +35,13 @@ export async function Hero() {
           <Link href="/dashboard">
             <Button variant="secondary">
               Dashboard
-              <LayoutDashboard className="ml-1 h-4 w-4" />
+              <LayoutDashboardIcon className="h-4 w-4" />
             </Button>
           </Link>
         ) : (
           <Button variant="secondary" onClick={handleSignIn}>
             Sign in with Github
-            <GithubIcon className="ml-1 h-4 w-4" />
+            <GithubIcon className="h-4 w-4" />
           </Button>
         )}
       </div>
